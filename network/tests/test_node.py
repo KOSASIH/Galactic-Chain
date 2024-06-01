2632c493ce1850e72752fcb2610eb1d2dfcddb3c
KOSASIH <kosasihg88@gmail.com>
2024-06-01 14:20:42 +0700

    Create test_node.py

---
+++ network/tests/test_node.py
@@ -0,0 +1,11 @@
+import unittest
+from node.node import Node
+
+class TestNode(unittest.TestCase):
+    def test_add_transaction(self):
+        # Test adding a transaction to the node
+        pass
+
+    def test_add_block(self):
+        # Test adding a block to the node
+        pass
