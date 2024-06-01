953e829683ea917959981ecff7d87ed0a24acdc0
KOSASIH <kosasihg88@gmail.com>
2024-06-01 14:19:50 +0700

    Create proof_of_stake.py

---
+++ network/consensus/proof_of_stake.py
@@ -0,0 +1,42 @@
+import hashlib
+import random
+from typing import List
+
+class ProofOfWork:
+    def __init__(self, blockchain):
+        self.blockchain = blockchain
+
+    def validate_block(self, block):
+        # Implement Proof of Work consensus algorithm
+        pass
+
+class ProofOfStake:
+    def __init__(self, blockchain, validators: List[str]):
+        self.blockchain = blockchain
+        self.validators = validators
+        self.stake_weights = self.calculate_stake_weights()
+
+    def calculate_stake_weights(self):
+        # Calculate the stake weights for each validator
+        # For simplicity, assume each validator has an equal stake
+        stake_weights = {validator: 1.0 for validator in self.validators}
+        return stake_weights
+
+    def select_validator(self):
+        # Select a random validator based on their stake weight
+        random_validator = random.choices(list(self.stake_weights.keys()), weights=self.stake_weights.values())[0]
+        return random_validator
+
+    def validate_block(self, block):
+        # Implement Proof of Stake consensus algorithm
+        validator = self.select_validator()
+        block_hash = hashlib.sha256(str(block).encode()).hexdigest()
+        validator_hash = hashlib.sha256(validator.encode()).hexdigest()
+        combined_hash = hashlib.sha256((block_hash + validator_hash).encode()).hexdigest()
+
+        # Check if the combined hash meets the target difficulty
+        target_difficulty = self.blockchain.target_difficulty
+        if int(combined_hash, 16) < target_difficulty:
+            return True
+        else:
+            return False
