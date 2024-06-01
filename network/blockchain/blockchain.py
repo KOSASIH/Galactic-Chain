68fd0c9cb766bc3f4ea4ddf6218a0d086b6e9bea
KOSASIH <kosasihg88@gmail.com>
2024-06-01 14:15:09 +0700

    Create blockchain.py

---
+++ network/blockchain/blockchain.py
@@ -0,0 +1,9 @@
+from.blockchain_storage import BlockchainStorage
+
+class Blockchain:
+    def __init__(self, blockchain_storage):
+        self.blockchain_storage = blockchain_storage
+
+    def add_block(self, block):
+        # Call blockchain_storage to add block
+        pass
